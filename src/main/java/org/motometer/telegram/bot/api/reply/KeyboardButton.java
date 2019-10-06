b7a472ffc184c248aec39ed36095c24f2d844b15
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-06 20:17:17 +0300

    Implement send message API.

--- src/main/java/org/motometer/telegram/bot/api/reply/KeyboardButton.java
+++ src/main/java/org/motometer/telegram/bot/api/reply/KeyboardButton.java
@@ -1,5 +1,26 @@
 package org.motometer.telegram.bot.api.reply;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.immutables.value.Value;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @see <a href="https://core.telegram.org/bots/api#keyboardbutton">KeyboardButton</a>
+ */
+@Value.Immutable
 public interface KeyboardButton {
 
+    @Value.Parameter
+    @JsonProperty("text")
+    String text();
+
+    @Nullable
+    @Value.Parameter
+    @JsonProperty("request_contact")
+    Boolean requestContact();
+
+    @Nullable
+    @Value.Parameter
+    @JsonProperty("request_location")
+    Boolean requestLocation();
 }
--- src/main/java/org/motometer/telegram/bot/api/reply/ReplyKeyboardMarkup.java
+++ src/main/java/org/motometer/telegram/bot/api/reply/ReplyKeyboardMarkup.java
@@ -1,17 +1,29 @@
 package org.motometer.telegram.bot.api.reply;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.immutables.value.Value;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+/**
+ * @see <a href="https://core.telegram.org/bots/api#replykeyboardmarkup">ReplyKeyboardMarkup</a>
+ */
 @Value.Immutable
 public interface ReplyKeyboardMarkup extends ReplyMarkup {
 
+    @JsonProperty("keyboard")
     List<List<KeyboardButton>> keyboard();
 
+    @Nullable
+    @JsonProperty("resize_keyboard")
     Boolean resizeKeyboard();
 
+    @Nullable
+    @JsonProperty("one_time_keyboard")
     Boolean oneTimeKeyboard();
 
+    @Nullable
+    @JsonProperty("selective")
     Boolean selective();
 }
