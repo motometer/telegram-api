cca9932df8734d851c145d19a967b67d21b53511
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-09 11:54:46 +0300

    Message API

--- src/main/java/org/motometer/telegram/bot/api/message/Audio.java
+++ src/main/java/org/motometer/telegram/bot/api/message/Audio.java
@@ -1,7 +1,40 @@
 package org.motometer.telegram.bot.api.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.immutables.value.Value;
+import org.jetbrains.annotations.Nullable;
+
 /**
- * @see <a href="">Audio</a>
+ * @see <a href="https://core.telegram.org/bots/api#audio">Audio</a>
  */
+@Value.Immutable
+@JsonDeserialize(builder = ImmutableAudio.Builder.class)
 public interface Audio {
+
+    @JsonProperty("file_id")
+    String fileId();
+
+    @JsonProperty("duration")
+    int duration();
+
+    @Nullable
+    @JsonProperty("performer")
+    String performer();
+
+    @Nullable
+    @JsonProperty("title")
+    String title();
+
+    @Nullable
+    @JsonProperty("mime_type")
+    String mimeType();
+
+    @Nullable
+    @JsonProperty("file_size")
+    Integer file_size();
+
+    @Nullable
+    @JsonProperty("thumb")
+    PhotoSize thumb();
 }
