d40883aa0e16a29acbd68fa6d48a5d62a5b63d7c
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-10 00:42:16 +0300

    Message API

--- src/main/java/org/motometer/telegram/bot/api/message/Voice.java
+++ src/main/java/org/motometer/telegram/bot/api/message/Voice.java
@@ -1,7 +1,28 @@
 package org.motometer.telegram.bot.api.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.immutables.value.Value;
+import org.jetbrains.annotations.Nullable;
+
 /**
- * @see <a href="">Voice</a>
+ * @see <a href="https://core.telegram.org/bots/api#voice">Voice</a>
  */
+@Value.Immutable
+@JsonDeserialize(builder = ImmutableVoice.Builder.class)
 public interface Voice {
+
+    @JsonProperty("file_id")
+    String fileId();
+
+    @JsonProperty("duration")
+    int duration();
+
+    @Nullable
+    @JsonProperty("mime_type")
+    String mimeType();
+
+    @Nullable
+    @JsonProperty("file_size")
+    Integer fileSize();
 }
