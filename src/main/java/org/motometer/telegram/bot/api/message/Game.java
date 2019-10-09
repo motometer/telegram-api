c91c0aa4d959bae6359fb8f34163f07515376042
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-10 00:53:54 +0300

    Message API

--- src/main/java/org/motometer/telegram/bot/api/message/Game.java
+++ src/main/java/org/motometer/telegram/bot/api/message/Game.java
@@ -1,7 +1,37 @@
 package org.motometer.telegram.bot.api.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.immutables.value.Value;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
 /**
- * @see <a href="">Game</a>
+ * @see <a href="https://core.telegram.org/bots/api#game">Game</a>
  */
+@Value.Immutable
+@JsonDeserialize(builder = ImmutableGame.Builder.class)
 public interface Game {
+
+    @JsonProperty("title")
+    String title();
+
+    @JsonProperty("description")
+    String description();
+
+    @JsonProperty("photo")
+    List<PhotoSize> photos();
+
+    @Nullable
+    @JsonProperty("text")
+    String text();
+
+    @Nullable
+    @JsonProperty("text_entities")
+    List<MessageEntity> textEntities();
+
+    @Nullable
+    @JsonProperty("animation")
+    Animation animation();
 }
