0800d17038e65d3d0a45b53bec5b024914d36ed4
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-20 11:08:56 +0300

    Add exception to interface signature. (#11)
    Update javadocs.
    Set source compatibility to Java 8.

--- src/main/java/org/motometer/telegram/bot/TelegramApiException.java
+++ src/main/java/org/motometer/telegram/bot/TelegramApiException.java
@@ -1,12 +1,19 @@
 package org.motometer.telegram.bot;
 
+/**
+ * Root exception thrown by any method of {@link Bot}
+ */
 public class TelegramApiException extends RuntimeException {
 
-    public TelegramApiException(Throwable e) {
-        super(e);
+    public TelegramApiException(Throwable cause) {
+        super(cause);
     }
 
     public TelegramApiException(String message) {
         super(message);
+    }
+
+    public TelegramApiException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
