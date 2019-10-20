9338739f583e6d49e48bd1702143c80a7be97f7e
Viacheslav Bychkovskyi <vbychkovskyy@gmail.com>
2019-10-20 18:24:41 +0300

    Bot implementation.

--- bot-jdk8/src/main/java/org/motometer/telegram/bot/jdk8/BotBuilder.java
+++ bot-jdk8/src/main/java/org/motometer/telegram/bot/jdk8/BotBuilder.java
@@ -2,6 +2,9 @@ package org.motometer.telegram.bot.jdk8;
 
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapterFactory;
+import lombok.SneakyThrows;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.motometer.telegram.bot.Bot;
 
 import java.util.ServiceLoader;
@@ -18,13 +21,26 @@ public class BotBuilder {
         host = DEFAULT_HOST;
     }
 
+    @SneakyThrows
     public Bot build() {
         GsonBuilder gsonBuilder = new GsonBuilder();
         for (TypeAdapterFactory factory : ServiceLoader.load(TypeAdapterFactory.class)) {
             gsonBuilder.registerTypeAdapterFactory(factory);
         }
 
-        return new DefaultBot(new BotTemplate(baseUri(), gsonBuilder.create()));
+        HttpClient httpClient = new HttpClient(new SslContextFactory.Client());
+
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                httpClient.stop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }));
+        httpClient.start();
+
+        return new DefaultBot(new BotTemplate(httpClient, baseUri(), gsonBuilder.create()));
     }
 
     private String baseUri() {
